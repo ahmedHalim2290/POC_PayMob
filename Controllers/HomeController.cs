86db939b242cc87fd67dfd895eede48ca4282d7d
ahmed Abd El-Halim <37662016+ahmedHalim2290@users.noreply.github.com>
2025-01-23 10:58:55 +0200

    ADD THANK YOU PAGE

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using POC_PayMob.Filters;
 using POC_PayMob.Models;
 using POC_PayMob.Services;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace POC_PayMob.Controllers {
     public class HomeController : Controller {
@@ -12,12 +14,22 @@ namespace POC_PayMob.Controllers {
             _paymobService = paymobService;
         }
 
-        public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index([FromQuery] string hmac)
         {
+            var _hmac = hmac;
             var paymentToken = await _paymobService.GetPaymentTokenAsync(10, "EGP", 232523234);
             var model = new { PaymentToken = paymentToken };
             return View(model);
 
         }
+        [ServiceFilter(typeof(HmacValidationFilter))] // Apply the HMAC filter
+        public async Task<IActionResult> Thank()
+        {
+
+            return View();
+
+        }
     }
 }
+
