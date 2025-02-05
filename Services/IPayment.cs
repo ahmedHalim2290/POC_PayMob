366adfe4024c6558b089ba61b1cbb6ae59b0a36c
ahmed Abd El-Halim <37662016+ahmedHalim2290@users.noreply.github.com>
2025-02-05 07:40:10 +0200

    enhancement for interface

--- Services/IPayment.cs
+++ Services/IPayment.cs
@@ -2,8 +2,15 @@
 
 namespace POC_PayMob.Services {
     public interface IPayment {
-        Task<dynamic> CaptureTransactionAsync(ProcessRequestDto captureRequest);
-        Task<string> GetClientSecretAsync(OrderRequestDto orderRequest);
+        Task<dynamic> CaptureTransactionAsync(ProcessRequestDto captureDto);
+        Task<string> GetClientSecretAsync(OrderRequestDto payload, bool IsAuth);
+        Task<dynamic> GetTransactionByMerchantOrderIdAsync(string MerchantOrderId);
+        Task<dynamic> GetTransactionByOrderIdAsync(int orderId);
+        Task<dynamic> GetTransactionByTrxIdAsync(int transactionID);
+        Task<string> GetWithTokenAsync(object data, string url);
         Task<string> PostAsync(object data, string url);
+        Task<string> PostWithTokenAsync(object data, string url);
+        Task<dynamic> RefundTransactionAsync(ProcessRequestDto refundDto);
+        Task<dynamic> VoidTransactionAsync(VoidRequestDto voidDto);
     }
 }
--- Services/PaymobService.cs
+++ Services/PaymobService.cs
@@ -10,20 +10,16 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace POC_PayMob.Services {
-    public class PaymobService {
+    public class PaymobService : IPayment {
 
-       
+
         private readonly HttpClient _httpClient;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly PaymobOptions _options;
-
-
-
-
-        public PaymobService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor,IOptions<PaymobOptions> options)
+        public PaymobService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<PaymobOptions> options)
         {
             _options = options.Value;
-        
+
             _httpClient = httpClient;
             _httpContextAccessor = httpContextAccessor;
 
@@ -31,7 +27,7 @@ namespace POC_PayMob.Services {
 
         public async Task<string> GetClientSecretAsync(OrderRequestDto payload, bool IsAuth)
         {
-            var IntegrationID = IsAuth?_options.AuthIntegrationID:_options.StandAloneIntegrationID;
+            var IntegrationID = IsAuth ? _options.AuthIntegrationID : _options.StandAloneIntegrationID;
 
             payload.PaymentMethods = new object[] { "card", IntegrationID };
             var clientSecretResponse = await PostAsync(payload, _options.PaymentTransactionURL);
@@ -44,57 +40,58 @@ namespace POC_PayMob.Services {
         {
             return await PostAsync(captureDto, _options.CaptureTransactionURL);
         }
+
         public async Task<dynamic> VoidTransactionAsync(VoidRequestDto voidDto)
         {
             return await PostAsync(voidDto, _options.VoidTransactionURL);
         }
+
         public async Task<dynamic> RefundTransactionAsync(ProcessRequestDto refundDto)
         {
             return await PostAsync(refundDto, _options.RefundTransactionURL);
         }
-      
-        private async Task<string> GetAuthTokenAsync()
-        {
-            var result= await PostWithTokenAsync(new { api_key = _options.APIKey }, _options.AuthTokenURL);
-            var token = JsonConvert.DeserializeObject<dynamic>(result).token;
-            return token;
-        }
+
         public async Task<dynamic> GetTransactionByMerchantOrderIdAsync(string MerchantOrderId)
         {
             var token = await GetAuthTokenAsync();
-            return await PostWithTokenAsync(new { auth_token =token,merchant_order_id= MerchantOrderId },_options.TransacrionByMerchantOrderIdURL);
+            return await PostWithTokenAsync(new { auth_token = token, merchant_order_id = MerchantOrderId }, _options.TransacrionByMerchantOrderIdURL);
         }
+
         public async Task<dynamic> GetTransactionByOrderIdAsync(int orderId)
         {
             var token = await GetAuthTokenAsync();
-            return await PostWithTokenAsync(new { auth_token = token, order_id= orderId }, _options.TransacrionByOrderIdURL);
+            return await PostWithTokenAsync(new { auth_token = token, order_id = orderId }, _options.TransacrionByOrderIdURL);
         }
+
         public async Task<dynamic> GetTransactionByTrxIdAsync(int transactionID)
         {
             var token = await GetAuthTokenAsync();
-            return await GetWithTokenAsync(new { auth_token = token },_options.TransacrionByTrxIdURL+"/"+transactionID );
+            return await GetWithTokenAsync(new { auth_token = token }, _options.TransacrionByTrxIdURL + "/" + transactionID);
         }
+
         public async Task<string> PostWithTokenAsync(object data, string url)
         {
             var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
-           
-        
+
+
         }
+
         public async Task<string> GetWithTokenAsync(object data, string url)
         {
             var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Content = content;
             var response = await _httpClient.SendAsync(request);
-           
+
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
-           
+
 
         }
+
         public async Task<string> PostAsync(object data, string url)
         {
             var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
@@ -102,16 +99,23 @@ namespace POC_PayMob.Services {
             request.Headers.Add("Authorization", "Token " + _options.authToken);
             request.Content = content;
             var response = await _httpClient.SendAsync(request);
-           /* //For Testing
-           if (!response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Error: {response.StatusCode}");
-                Console.WriteLine($"Response: {responseContent}");
-                throw new HttpRequestException($"Request failed with status code {response.StatusCode}: {responseContent}");
-            }*/
+            /* //For Testing
+            if (!response.IsSuccessStatusCode)
+             {
+                 var responseContent = await response.Content.ReadAsStringAsync();
+                 Console.WriteLine($"Error: {response.StatusCode}");
+                 Console.WriteLine($"Response: {responseContent}");
+                 throw new HttpRequestException($"Request failed with status code {response.StatusCode}: {responseContent}");
+             }*/
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task<string> GetAuthTokenAsync()
+        {
+            var result = await PostWithTokenAsync(new { api_key = _options.APIKey }, _options.AuthTokenURL);
+            var token = JsonConvert.DeserializeObject<dynamic>(result).token;
+            return token;
         }
     }
 
